ef6f86d7844a181a470d24bc90c9a4972309bfe7
Amol Paliwal <amolpaliwalct19@acropolis.in>
2021-11-10 23:55:28 +0530

    new

--- ASSIGNMENT/49.py
+++ ASSIGNMENT/49.py
@@ -1,1 +1,3 @@
-ASSIGNMENT(70)/
+def f(n):return n*n +1
+a=int(input("enter no:"))
+print(f(a))
---
+++ ASSIGNMENT/50.py
@@ -0,0 +1,5 @@
+s = "str = 'Y-tatata-acropolis: 0.8475"
+m = s.find(":")
+y = s[m+1:]
+y = float(y)
+print(y)
